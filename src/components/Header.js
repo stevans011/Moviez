d86bf7c2cf24a74da1a18d612396d9bac41279d7
deborawein <debiwein@gmail.com>
2022-11-22 12:23:59 +1100

    added logo and tested login system

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,15 @@
-// import { Navigation } from "./Navigation"
-// import { Link } from "react-router-dom"
-
-// export function Header(props) {
-//     return (
-//         <header className="navbar bg-light navbar-expand-lg">
-//             <div className="container-fluid">
-//                 <Link className="navbar-brand" to="/">{ props.title }</Link>
-//                 <div className="collapse navbar-collapse">
-//                     <Navigation items={ props.headernav } />
-//                 </div>
-//             </div>
-//         </header>
-//     )
-// }
-
-
 import { LeftNav } from "./LeftNav"
 import { SearchBar } from "./SearchBar"
 import { RightNav } from "./RightNav"
 import { Link } from 'react-router-dom'
+import logo from '../images/logo.png'
 
 export function Header(props) {
     return (
         <header className="navbar navbar-expand-lg navbar-light bg-danger">
             <div className="container-fluid">
                 {/* Navbar brand */}
-                <Link className="navbar-brand fw-bold" to="/">BRAND</Link>
+                <Link className="navbar-brand" to="/"><img src={logo} width="130"></img></Link>
 
                 {/* Toggle button */}
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,32 +1,32 @@
-import {useState,useEffect} from 'react'
-import { NavLink } from 'react-router-dom'
+// import {useState,useEffect} from 'react'
+// import { NavLink } from 'react-router-dom'
 
-export function Navigation ( props ) {
-    const[ navItems, setNavItems ] = useState([])
+// export function Navigation ( props ) {
+//     const[ navItems, setNavItems ] = useState([])
 
-    useEffect( () => {
-        // if( navItems.length == 0 ) {
-        //     console.log( props.items )
-        //     setNavItems( props.items )
-        // }
-        setNavItems( props.items )
-    }, [ props.items ] )
+//     useEffect( () => {
+//         // if( navItems.length == 0 ) {
+//         //     console.log( props.items )
+//         //     setNavItems( props.items )
+//         // }
+//         setNavItems( props.items )
+//     }, [ props.items ] )
 
-    if ( navItems.length > 0 ) {
-        // NavigationLinks collection return html code for each item in the array
-        const NavigationLinks = navItems.map( ( item, itemKey ) => {
-            return(
-                <NavLink to={item.path} className="nav-link" key={itemKey} >{ item.name }</NavLink>
-            )
-        })
+//     if ( navItems.length > 0 ) {
+//         // NavigationLinks collection return html code for each item in the array
+//         const NavigationLinks = navItems.map( ( item, itemKey ) => {
+//             return(
+//                 <NavLink to={item.path} className="nav-link" key={itemKey} >{ item.name }</NavLink>
+//             )
+//         })
 
-        return (
-            <nav className="navbar-nav me-auto mb-2 mb-lg-0">
-                { NavigationLinks }
-            </nav>
-        )
-    }
-    else {
-        return null
-    }
-}
+//         return (
+//             <nav className="navbar-nav me-auto mb-2 mb-lg-0">
+//                 { NavigationLinks }
+//             </nav>
+//         )
+//     }
+//     else {
+//         return null
+//     }
+// }
